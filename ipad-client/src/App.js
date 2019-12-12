2079940abea728b93a9d42ce74db9e5b8b42f573
Axel Engström <axel@axelca.com>
2019-12-12 14:25:20 +0100

    testing session over internet

--- ipad-client/src/App.js
+++ ipad-client/src/App.js
@@ -6,8 +6,7 @@ import minus from "./minus.svg";
 import plus from "./plus.svg";
 
 const App = () => {
-  //const socket = openSocket("https://e6e4d7de.ngrok.io");
-  const socket = openSocket("http://localhost:8000");
+  const socket = openSocket("https://aabae799.ngrok.io");
   const [peopleValue, setPeopleValue] = useState(0.5); // sätt inte? ett initialt värde för vad användarna tycker
   const [wasClicked, setWasClicked] = useState(false);
 
@@ -33,31 +32,34 @@ const App = () => {
 
   return (
     <div className="wrapper">
-    <div className="App">
-      <h1>Control the mood!</h1>
-      <h3>Be a part of controlling the ambience by voting to increase or decrease the energy level</h3>
-      <div className={wasClicked ? "controls wasClicked" : "controls"}>
-        <button
-          className="controls__control controls__minus"
-          onClick={() => handleClick("subtract")}
-        >
-          <img src={minus} />
-        </button>
-        <button
-          className="controls__control controls__plus"
-          onClick={() => handleClick("add")}
-        >
-          <img src={plus} />
-        </button>
+      <div className="App">
+        <h1>Control the mood!</h1>
+        <h3>
+          Be a part of controlling the ambience by voting to increase or
+          decrease the energy level
+        </h3>
+        <div className={wasClicked ? "controls wasClicked" : "controls"}>
+          <button
+            className="controls__control controls__minus"
+            onClick={() => handleClick("subtract")}
+          >
+            <img src={minus} />
+          </button>
+          <button
+            className="controls__control controls__plus"
+            onClick={() => handleClick("add")}
+          >
+            <img src={plus} />
+          </button>
+        </div>
+        <div className="status">
+          Energy:{" "}
+          <span className="status__peopleValue">
+            {Math.round(peopleValue * 100)}
+          </span>{" "}
+          / 100
+        </div>
       </div>
-      <div className="status">
-        Energy:{" "}
-        <span className="status__peopleValue">
-          {Math.round(peopleValue * 100)}
-        </span>{" "}
-        / 100
-      </div>
-    </div>
     </div>
   );
 };
